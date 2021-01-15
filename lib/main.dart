09381562519cc52f77f6e218708a9f4928fc1c6a
Carlos Viteri <carlos.viteri@verndale.com>
2021-01-15 16:06:44 -0500

    Add default case

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'matches.dart';
 import 'bet.dart';
 import 'profile.dart';
 
-
 void main() {
   runApp(MyApp());
 }
@@ -16,52 +15,51 @@ class MyApp extends StatelessWidget {
     return CupertinoApp(
         title: 'Pollaza',
         home: CupertinoTabScaffold(
-                tabBar: CupertinoTabBar(
-                  items: <BottomNavigationBarItem> [
-                    BottomNavigationBarItem(
-                      icon: Icon(CupertinoIcons.list_number),
-                      label: 'Posiciones',
-                    ),
-                    BottomNavigationBarItem(
-                      icon: Icon(CupertinoIcons.bars),
-                      label: 'Resultados',
-                    ),
-                    BottomNavigationBarItem(
-                      icon: Icon(CupertinoIcons.pencil_circle),
-                      label: 'Apuestas',
-                    ),
-                    BottomNavigationBarItem(
-                      icon: Icon(CupertinoIcons.person_alt_circle),
-                      label: 'Perfil',
-                    ),
-                  ],
-                ),
-                tabBuilder: (BuildContext context, int index) {
-                  CupertinoTabView returnValue;
-                  switch (index){
-                    case 0:
-                      return SafeArea(child: Container(child: Rankings(phase: 2)));
-                    break;
-                    case 1:
-                      return SafeArea(child: Container(child: Matches(phase: 2)));
-                    break;
-                    case 2:
-                      return SafeArea(child: Container(child: Bet(phase: 2)));
-                    break;
-                    case 3:
-                      return SafeArea(child: Container(child: Profile()));
-                    break;
-
-                  }
-                  
-              },
-          )
-        );
-        // home: CupertinoPageScaffold(
-        //     navigationBar: CupertinoNavigationBar(
-        //       middle: Text('Pollaza'),
-        //     ),
-        //     child: SafeArea(child: Container(child: Matches(phase: 2)))));
-
+          tabBar: CupertinoTabBar(
+            items: <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(CupertinoIcons.list_number),
+                label: 'Posiciones',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(CupertinoIcons.bars),
+                label: 'Resultados',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(CupertinoIcons.pencil_circle),
+                label: 'Apuestas',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(CupertinoIcons.person_alt_circle),
+                label: 'Perfil',
+              ),
+            ],
+          ),
+          tabBuilder: (BuildContext context, int index) {
+            switch (index) {
+              case 0:
+                return SafeArea(child: Container(child: Rankings(phase: 2)));
+                break;
+              case 1:
+                return SafeArea(child: Container(child: Matches(phase: 2)));
+                break;
+              case 2:
+                return SafeArea(child: Container(child: Bet(phase: 2)));
+                break;
+              case 3:
+                return SafeArea(child: Container(child: Profile()));
+                break;
+              default:
+                return SafeArea(
+                  child: Text('LOL'),
+                );
+            }
+          },
+        ));
+    // home: CupertinoPageScaffold(
+    //     navigationBar: CupertinoNavigationBar(
+    //       middle: Text('Pollaza'),
+    //     ),
+    //     child: SafeArea(child: Container(child: Matches(phase: 2)))));
   }
- }
+}
