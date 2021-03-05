589eb24d92bf2f9958c7d77908c3b1715b979f5f
Germán Villacreces <german.villacreces@gmail.com>
2021-03-04 23:33:04 -0500

    Almost fixed logout and fixed avatars

--- lib/rankings.dart
+++ lib/rankings.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:ots/ots.dart';
 import 'api_provider.dart';
 import 'drawer.dart';
+import 'globals.dart';
+import 'dart:developer';
 
 class Rankings extends StatefulWidget {
   Rankings({@required this.phase});
@@ -31,25 +33,39 @@ class _RankingsState extends State<Rankings> with AfterLayoutMixin<Rankings> {
     int count = 1;
     var data = positions.map((user) => {
           'name': user['user']['fullName'],
+          'photo': user['user']['photo'] != null
+              ? "http:" +
+                  user['user']['photo']["fields"]["file"]["url"].toString()
+              : Globals.defaultAvatar,
           'res': user['winner'].toString(),
           'mar': user['score'].toString(),
           'puntos': user['total'].toString()
         });
+    inspect(positions);
 
     List<TableRow> rows = [
       TableRow(children: [
+        Text(" "),
         Text(" "),
         Text("Nombre",
             style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
         Text("R", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
         Text("M", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-        Text("Puntos",
-            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold))
+        Text("Pts", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold))
       ])
     ];
     for (var result in data) {
       rows.add(TableRow(children: [
         Text((count++).toString()),
+        new Container(
+            width: 30.0,
+            height: 30.0,
+            padding: EdgeInsets.all(10.0),
+            decoration: new BoxDecoration(
+                shape: BoxShape.circle,
+                image: new DecorationImage(
+                    fit: BoxFit.fill,
+                    image: new NetworkImage(result["photo"])))),
         Text(result["name"]),
         new Text(result["res"]),
         new Text(result["mar"]),
@@ -69,20 +85,22 @@ class _RankingsState extends State<Rankings> with AfterLayoutMixin<Rankings> {
                     style:
                         TextStyle(fontWeight: FontWeight.w700, fontSize: 20)),
                 Container(
-                  padding: EdgeInsets.all(12.0),
+                  padding: EdgeInsets.all(15.0),
                   child: Table(
+                    defaultVerticalAlignment: TableCellVerticalAlignment.middle,
                     children: rows,
                     border: TableBorder(
                         horizontalInside: BorderSide(
                             width: 1,
-                            color: CupertinoColors.activeBlue,
+                            color: CupertinoColors.lightBackgroundGray,
                             style: BorderStyle.solid)),
                     columnWidths: Map.from({
-                      0: FractionColumnWidth(0.1),
-                      1: FractionColumnWidth(0.5),
-                      2: FractionColumnWidth(0.1),
+                      0: FractionColumnWidth(0.05),
+                      1: FractionColumnWidth(0.1),
+                      2: FractionColumnWidth(0.55),
                       3: FractionColumnWidth(0.1),
-                      4: FractionColumnWidth(0.2)
+                      4: FractionColumnWidth(0.1),
+                      5: FractionColumnWidth(0.1)
                     }),
                   ),
                 ),
